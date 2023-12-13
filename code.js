c028aa690acaca86b76b385327a754311bb26614
DiAlSoJa <sotodiego37@gmail.com>
2023-12-13 03:57:06 -0600

    salavergaasdfa

--- code.js
+++ code.js
@@ -1,11 +1,1 @@
-const toClose = document.getElementById("close");
-const toActive= document.getElementById("active");
-// const form = document.querySelector(".formulario");
-// const nombre = document.getElementById("nombre");
-// const email = document.getElementById("email");
-// const numero = document.getElementById("numero");
-// const mensage = document.getElementById("mensage");
-
-toClose.addEventListener("click",()=>document.querySelector(".navigation-container").classList.remove("open"));
-toActive.addEventListener("click",()=>document.querySelector(".navigation-container").classList.add("open"));
-
+console.log("lol")
